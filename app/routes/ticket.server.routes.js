f06d610d7ea85d3b71439995ea48deebf899abb7
Radoslav Ivanov <rivanov@gmail.com>
2015-07-28 18:10:24 +0300

    Add sendmail route/nodemailer functionality

--- app/routes/ticket.server.routes.js
+++ app/routes/ticket.server.routes.js
@@ -5,6 +5,9 @@ var tickets = require('../../app/controllers/tickets.server.controller'),
 
 
 module.exports = function(app) {
+	app.route('/api/sendmail')
+		.get(tickets.sendEmail);
+
 	app.route('/api/ticket/latest')
 		.get(users.requiresLogin, tickets.latest);
 	
